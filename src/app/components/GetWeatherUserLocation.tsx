aa00f183fd69a444b3c19e4e6a7d40a305f8e13e
Rohit Shrestha <106756449+RohitSth@users.noreply.github.com>
2024-05-15 22:02:45 +0545

    fixed the button

--- src/app/components/GetWeatherUserLocation.tsx
+++ src/app/components/GetWeatherUserLocation.tsx
@@ -3,9 +3,13 @@
 import Weather from "./Weather";
 import { useState } from "react";
 
+import { FaLocationCrosshairs } from "react-icons/fa6";
+
 export default function GetWeatherUserLocation({ apiKey }: { apiKey: string }) {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
+
+  const [permissionDenied, setPermissionDenied] = useState<boolean>(false);
 
   // Ask for permission to get user location
   const getUserLocation = () => {
@@ -19,37 +23,48 @@ export default function GetWeatherUserLocation({ apiKey }: { apiKey: string }) {
     }
   };
 
-  const showPosition = (position: any) => {
-    alert(position.coords.latitude + ", " + position.coords.longitude); // Concatenate latitude and longitude into a single string
-  };
-
   const showError = (error: any) => {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        alert("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        alert("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        alert("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        alert("An unknown error occurred.");
-        break;
+    if (error.code === error.PERMISSION_DENIED) {
+      setPermissionDenied(true);
+    } else {
+      switch (error.code) {
+        case error.POSITION_UNAVAILABLE:
+          alert("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          alert("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERROR:
+          alert("An unknown error occurred.");
+          break;
+      }
     }
   };
 
   return (
     <div className="flex flex-col gap-y-2">
-      <button
-        onClick={getUserLocation}
-        className="rounded-md bg-white p-2 text-black"
-      >
-        Allow Location Access
-      </button>
-      {latitude !== null && longitude !== null && (
+      {permissionDenied ? (
+        <button
+          onClick={getUserLocation}
+          className="rounded-lg bg-gray-500 w-fit p-2 text-black"
+          disabled
+        >
+          <FaLocationCrosshairs />
+        </button>
+      ) : (
+        <button
+          onClick={getUserLocation}
+          className="rounded-lg bg-white w-fit p-2 text-black"
+        >
+          <FaLocationCrosshairs />
+        </button>
+      )}
+      {latitude !== null && longitude !== null && !permissionDenied ? (
         <Weather latitude={latitude} longitude={longitude} apiKey={apiKey} />
+      ) : (
+        <div>
+          <p className="text-black dark:text-white">Demo Data when access</p>
+        </div>
       )}
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
   const weatherApiKey = process.env.WEATHER_API as string;
   return (
     <div className="h-full flex gap-x-4 px-4 mx-auto max-w-7xl sm:px-6">
-      <div className="flex-1" style={{ flex: 0.618 }}>
+      <div className="w-80">
         <div>
           <GetWeatherUserLocation apiKey={weatherApiKey} />
         </div>
       </div>
-      <div className="flex-1 shadow shadow-white rounded" style={{ flex: 1 }}>
+      <div className="flex-1 shadow shadow-white rounded">
         <div className="flex">Okaayy</div>
       </div>
     </div>
